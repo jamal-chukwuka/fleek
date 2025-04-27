0c4f38d9a35b01b87776ab985adbe7777175baf8
Jamal Nezianya <jnezia1@hotmail.com>
2025-04-27 08:06:05 -0500

    Update vite config

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -11,6 +11,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',         
   server: {
     host: true,          // 👈 This allows network access (mobile view!)
     port: 5173           // Optional: specify the port
